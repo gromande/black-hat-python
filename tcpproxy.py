00c1d54e4f016fd9b70030e15fa7fa12643eda43
Ubuntu <ubuntu@dev.groman.com>
2017-11-21 21:02:19 +0000

    fixed some bugs

--- tcpproxy.py
+++ tcpproxy.py
@@ -19,7 +19,7 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
     while True:
         client_socket, client_addr = server.accept()
         print("[=>] Received incomming connection from %s:%d" %
-              (remote_host, remote_port))
+              (client_addr[0], client_addr[1]))
 
         proxy_thread = threading.Thread(target=proxy_handler,
             args=(client_socket,remote_host, remote_port, receive_first))
@@ -39,7 +39,7 @@ def main():
     remote_port = int(sys.argv[4])
     receive_first = sys.argv[5]
 
-    if "True" = receive_first:
+    if "True" == receive_first:
         receive_first = True
     else:
         receive_first = False
@@ -52,6 +52,8 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     remote_socket.connect((remote_host, remote_port))
 
+    print("[=>] Connected to %s:%d" % (remote_host, remote_port))
+
     #Receive data from the remote end if neccesary
     if receive_first:
         remote_buffer = receive_from(remote_socket)
@@ -63,7 +65,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
         #Send data to local client if required
         if len(remote_buffer):
             print("[<=] Sending %d bytes to localhost." % len(remote_buffer))
-            client_socket.send(remote_buffer)
+            client_socket.send(remote_buffer.encode())
 
     #Now lets loop and read from local, send to remote, read from remote,
     #send to local, and repeat
@@ -80,7 +82,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             local_buffer = request_handler(local_buffer)
 
             #send off the data to the remote host
-            remote_socket.send(local_buffer)
+            remote_socket.send(local_buffer.encode())
             print("[=>] Sent to remote")
 
         #receive back the response
@@ -94,8 +96,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             remote_buffer = response_handler(remote_buffer)
 
             #send response to local socket
-            client_socket.send(remote_buffer)
-
+            client_socket.send(remote_buffer.encode())
             print("[<=] Sent to localhost")
 
         #If no more data in either side, close connections
@@ -105,7 +106,44 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print("[*] No more data. Closing connections")
             break
 
+FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
 
+def hexdump(src, length=16):
+    N=0; result=''
+    while src:
+       s,src = src[:length],src[length:]
+       hexa = ' '.join(["%02X"%ord(x) for x in s])
+       s = s.translate(FILTER)
+       result += "%04X   %-*s   %s\n" % (N, length*3, hexa, s)
+       N+=length
+    print(result)
+
+def receive_from(connection):
+    buffer = ""
+    #we set a 2 second timeout
+    connection.settimeout(10)
+
+    try:
+        #Keep reading until there is no more data
+        #or we time out
+        while True:
+            data = connection.recv(4096)
+            print("Debug-> %d: %s" % (len(data), data.decode()))
+            if not data:
+                break
+            buffer += data.decode()
+    except Exception as ex:
+        print("[!] Warning: %s" % str(ex))
+
+    return buffer
+
+def request_handler(buffer):
+    #perform packet modifications
+    return buffer
+
+def response_handler(buffer):
+    #peform packet modifications
+    return buffer
 
 main()
 
